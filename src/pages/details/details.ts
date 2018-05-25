938412ec98e59e603ea12612dd204ffadb6049c1
Benjamin THOMAS <benjamin.thomas@ynov.com>
2018-05-25 16:19:58 +0200

    details + map

--- src/pages/details/details.ts
+++ src/pages/details/details.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 /**
  * Generated class for the DetailsPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
+
+declare var google;
 
 @IonicPage()
 @Component({
@@ -19,6 +20,13 @@ export class DetailsPage {
   date: String
   name: String
   image: String
+  lat: String
+  long: String
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     let concert = navParams.data.item
@@ -27,5 +35,28 @@ export class DetailsPage {
     this.date = concert.date
     this.name = concert.name
     this.image = concert.url_image
+    this.lat = concert.lat
+    this.long = concert.lon
+  }
+
+  ionViewDidLoad() {
+    this.initMap(this.lat, this.long);
+  }
+
+  initMap(lat, long) {
+    console.log(lat)
+    console.log(long)
+    this.map = new google.maps.Map(this.mapElement.nativeElement, {
+      zoom: 7,
+      center: { lat: parseFloat(lat), lng: parseFloat(long) }
+    });
+
+    var marker = new google.maps.Marker({
+      position: { lat: parseFloat(lat), lng: parseFloat(long) },
+      map: this.map,
+      title: this.name
+    });
+
+    this.directionsDisplay.setMap(this.map);
   }
 }
